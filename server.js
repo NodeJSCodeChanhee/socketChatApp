a427b4584043e82c9f2a2094d897a38f6a383de1
InChan Hwang <neogeoss1@gmail.com>
2016-09-01 00:24:49 +0900

    Moment error

--- server.js
+++ server.js
@@ -1,7 +1,9 @@
 var PORT = process.env.PORT || 3000;
+var moment = require('moment');
 
 var express = require('express');
 var app = express();
+
 
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
@@ -14,12 +16,16 @@ io.on('connection', function(socket) {
 	socket.on('message', function(message){
 		console.log('Message received:'  + message.text);
 
-	
+		message.timestamp = moment().valueOf();
+
 		io.emit('message', message);
 	});
 
+
+
 	socket.emit('message', {
-		text : 'Welcome to the chat application!'
+		text : 'Welcome to the chat application!',
+		timestamp: moment().valueOf()
 	});
 
 
