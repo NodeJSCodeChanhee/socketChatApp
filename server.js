5a4f8f63d4cfc20ad9745281747e1f2a7b0bbcb5
InChan Hwang <neogeoss1@gmail.com>
2016-09-01 00:36:17 +0900

    Nothing changed but this works

--- server.js
+++ server.js
@@ -1,39 +1,31 @@
 var PORT = process.env.PORT || 3000;
 var moment = require('moment');
-
 var express = require('express');
 var app = express();
-
-
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 
 app.use(express.static(__dirname + '/public'));
 
-io.on('connection', function(socket) {
+io.on('connection', function (socket) {
 	console.log('User connected via socket.io!');
 
-	socket.on('message', function(message){
-		console.log('Message received:'  + message.text);
+	socket.on('message', function (message) {
+		console.log('Message received: ' + message.text);
 
 		message.timestamp = moment().valueOf();
-
 		io.emit('message', message);
 	});
 
-
+	// timestamp property - JavaScript timestamp (milliseconds)
 
 	socket.emit('message', {
-		text : 'Welcome to the chat application!',
+		text: 'Welcome to the chat application!',
 		timestamp: moment().valueOf()
 	});
-
-
 });
 
-http.listen(PORT, function(){
+http.listen(PORT, function () {
 	console.log('Server started!');
 });
-
-
 
